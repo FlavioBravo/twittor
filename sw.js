339c918cc1fd90b7b5be055e693bf29b156e35ec
Flavio Bravo <f980534860@gmail.com>
2019-11-20 15:43:51 -0500

    fixing for iOs

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -52,8 +52,11 @@ self.addEventListener('activate', e => {
 
         keys.forEach( key => {
 
-            //statice-v4
-            if( key !== STATIC_CACHE && key.includes('static') ){
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
 
